cafd30c640064790ff24331d33ec2af22f425239
Liko <li_xie1992@hotmail.com>
2019-10-11 17:41:30 +0100

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,38 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @manager.employees << self
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        self.manager.name
+    end
+
+    def self.paid_over(salary_threshold)
+        self.all.select{|employee| employee.salary > salary_threshold }
+    end 
+
+    def self.find_by_department(department_name)
+        department_manager = Manager.all.find{|manager| manager.department == department_name}
+        Employee.all.find{|employee| employee.manager == department_manager }
+    end
+
+    def tax_bracket
+        Employee.all.select do |employee|
+            (employee.salary - self.salary).abs < 1000
+        end
+    end
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,17 @@ require 'pry'
 
 #Test your code here
 
+m1 = Manager.new("Jim", "HR", 34)
+m2 = Manager.new("Steve", "Finance", 32)
+m3 = Manager.new("Boris", "Politics", 45)
+
+e1 = Employee.new("Ed", 23000, m1)
+e2 = Employee.new("John", 23000, m2)
+e3 = Employee.new("Andy", 34000, m3)
+e4 = Employee.new("Matt", 33000, m1)
+e5 = Employee.new("Bob", 43000, m2)
+e6 = Employee.new("Nick", 42000, m1)
+
 
 binding.pry
 puts "done"
